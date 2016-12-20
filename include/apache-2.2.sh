3631074a2c283e2b7f2e44f5889ee4f20299fb15
kaneawk <kaneawk@gmail.com>
2016-12-20 20:38:45 +0800

    Fix misc

--- include/apache-2.2.sh
+++ include/apache-2.2.sh
@@ -50,12 +50,10 @@ Install_Apache22() {
   if [ "${Nginx_version}" == '4' -a ! -e "${web_install_dir}/sbin/nginx" ]; then
     sed -i 's/^#ServerName www.example.com:80/ServerName 0.0.0.0:80/' ${apache_install_dir}/conf/httpd.conf
     TMP_PORT=80
-    TMP_IP=${IPADDR}
   elif [[ "${Nginx_version}" =~ ^[1-3]$ ]] || [ -e "${web_install_dir}/sbin/nginx" ]; then
     sed -i 's/^#ServerName www.example.com:80/ServerName 127.0.0.1:88/' ${apache_install_dir}/conf/httpd.conf
     sed -i 's@^Listen.*@Listen 127.0.0.1:88@' ${apache_install_dir}/conf/httpd.conf
     TMP_PORT=88
-    TMP_IP=127.0.0.1
   fi
   sed -i "s@AddType\(.*\)Z@AddType\1Z\n    AddType application/x-httpd-php .php .phtml\n    AddType application/x-httpd-php-source .phps@" ${apache_install_dir}/conf/httpd.conf
   sed -i "s@#AddHandler cgi-script .cgi@AddHandler cgi-script .cgi .pl@" ${apache_install_dir}/conf/httpd.conf
@@ -75,7 +73,7 @@ ${wwwlogs_dir}/*apache.log {
   notifempty
   sharedscripts
   postrotate
-  [ -f ${apache_install_dir}/logs/httpd.pid ] && kill -USR1 \$\(cat ${apache_install_dir}/logs/httpd.pid\)
+  [ -e /var/run/httpd.pid ] && kill -USR1 \$\(cat /var/run/httpd.pid\)
   endscript
 }
 EOF
@@ -84,9 +82,9 @@ EOF
   cat > ${apache_install_dir}/conf/vhost/0.conf << EOF
 NameVirtualHost *:${TMP_PORT}
 <VirtualHost *:${TMP_PORT}>
-  ServerAdmin admin@linuxeye.com
+  ServerAdmin admin@example.com
   DocumentRoot "${wwwroot_dir}/default"
-  ServerName ${TMP_IP}
+  ServerName 127.0.0.1
   ErrorLog "${wwwlogs_dir}/error_apache.log"
   CustomLog "${wwwlogs_dir}/access_apache.log" common
 <Directory "${wwwroot_dir}/default">
@@ -117,6 +115,7 @@ EOF
   SetOutputFilter DEFLATE
 </IfModule>
 
+PidFile /var/run/httpd.pid
 ServerTokens ProductOnly
 ServerSignature Off
 Include conf/vhost/*.conf
